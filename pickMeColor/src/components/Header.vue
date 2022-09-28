d49c022dca2adcbe78e28cb8cb2f14eaad088150
MerlinAlex <merlinalex@qq.com>
2022-09-28 13:40:01 +0800

    fix: change mode button not display

--- pickMeColor/src/components/Header.vue
+++ pickMeColor/src/components/Header.vue
@@ -5,18 +5,19 @@ const isDark = useDark()
 const toggleDark = useToggle(isDark)
 </script>
 <template>
-  <nav flex="~" justify-between  w-cen m-au >
+  <nav flex="~" justify-between w-cen m-au>
     <!-- <img src="" alt="logo"> -->
     <div font="sans 700" text-40>Header</div>
-    <button @click="toggleDark()">
-      <i inline-block align-middle i="light:carbon-moon carbon-sun" />
+    <button class="icon-btn !outline-none" @click="toggleDark()">
+      <div v-if="isDark" i-carbon-moon />
+      <div v-else i-carbon-sun />
       <span class="ml-2">{{ isDark ? 'Dark' : 'Light' }}</span>
     </button>
   </nav>
 </template>
 
 <style scoped>
-.nav{
+.nav {
   margin: auto;
 }
 </style>
