a169690508dadf56b81f1e824134d7263e468ca1
xiayuhong <2298246596@qq.com>
2022-10-07 15:51:10 +0800

    feat:header 内容填充及样式

--- pickMeColor/src/components/ColorCard/index.vue
+++ pickMeColor/src/components/ColorCard/index.vue
@@ -1,82 +1,94 @@
 <script setup lang='ts'>
-import { reactive, ref, watch } from 'vue'
-import { ColorPicker } from 'vue3-ts-picker'
-import Slider from 'vue3-slider'
+import { reactive, ref, watch } from "vue";
+import { ColorPicker } from "vue3-ts-picker";
+import Slider from "vue3-slider";
 
 // 颜色的特征，距离和颜色
 interface colorType {
-  color: string
-  distance: number
+  color: string;
+  distance: number;
 }
 
-const colorPickerFlag = ref<boolean>(false)
+const colorPickerFlag = ref<boolean>(false);
 
-const color = ref<string>(' red, blue')
+const color = ref<string>(" red, blue");
 
 const colorArr = reactive<colorType[]>([
   {
-    color: 'red',
-    distance: -1
-  }
-])
+    color: "red",
+    distance: -1,
+  },
+]);
 
 // 缓存的颜色
-const cacheColor = ref('#ccc')
+const cacheColor = ref("#ccc");
 // 旋转
-const cachAngle = ref<number>(1)
-const angle = ref<string>('1deg')
+const cachAngle = ref<number>(1);
+const angle = ref<string>("1deg");
 const addcolor = (flag: boolean) => {
-  console.log(1)
+  console.log(1);
   if (flag) {
-    colorArr.push(
-      {
-        color: cacheColor.value,
-        distance: -1
-      }
-    )
-    console.log(colorArr)
+    colorArr.push({
+      color: cacheColor.value,
+      distance: -1,
+    });
+    console.log(colorArr);
   }
-  colorPickerFlag.value = false
-}
+  colorPickerFlag.value = false;
+};
 
-watch(() => cachAngle, (value, oldvalue) => {
-  if (cachAngle.value) {
-    angle.value = `${cachAngle.value}deg`
-    console.log(angle)
-  }
-  else {
-    angle.value = `${0}deg`
-  }
-}, { deep: true })
+watch(
+  () => cachAngle,
+  (value, oldvalue) => {
+    if (cachAngle.value) {
+      angle.value = `${cachAngle.value}deg`;
+      console.log(angle);
+    } else {
+      angle.value = `${0}deg`;
+    }
+  },
+  { deep: true }
+);
 
-watch(() => colorArr, (value, oldvalue) => {
-  let str = ''
-  for (let i = 0; i < colorArr.length; ++i) {
-    str += colorArr[i].color
-    if (colorArr[i].distance >= 0)
-      str += (` ${colorArr[i].distance}%`)
+watch(
+  () => colorArr,
+  (value, oldvalue) => {
+    let str = "";
+    for (let i = 0; i < colorArr.length; ++i) {
+      str += colorArr[i].color;
+      if (colorArr[i].distance >= 0) str += ` ${colorArr[i].distance}%`;
 
-    str += ','
-  }
-  color.value = str.substr(0, str.length - 1)
-  console.log(color)
-}, { deep: true })
+      str += ",";
+    }
+    color.value = str.substr(0, str.length - 1);
+    console.log(color);
+  },
+  { deep: true }
+);
 
 const selectColor = (color: string) => {
-  cacheColor.value = color
-}
+  cacheColor.value = color;
+};
 
 const delColor = (index: number) => {
-  colorArr.splice(index, 1)
-}
+  colorArr.splice(index, 1);
+};
 </script>
 
 <template>
   <div>
     <div class="card">
       <div class="cardBody-current">
-        <Slider v-model="cachAngle" orientation="circular" max="361" step="1" color="#FB278D"
-          track-color="rgba(255,255,255,0.4)" height="10" width="85%" />
+        <Slider
+          v-model="cachAngle"
+          orientation="circular"
+          max="361"
+          step="1"
+          color="#FB278D"
+          track-color="rgba(255,255,255,0.4)"
+          height="10"
+          width="85%"
+        />
       </div>
       <div class="cardFooter">
         <div class="angle">
@@ -84,8 +96,12 @@ const delColor = (index: number) => {
         </div>
         <div class="colors">
           <div v-for="(item, index) in colorArr" :key="index">
-            <div v-show="!colorPickerFlag" class="circular-current colorsItem"
-              :style="`background-color:${item.color};`" @click="colorPickerFlag = !colorPickerFlag" />
+            <div
+              v-show="!colorPickerFlag"
+              class="circular-current colorsItem"
+              :style="`background-color:${item.color};`"
+              @click="colorPickerFlag = !colorPickerFlag"
+            />
             <!-- <button @click="delColor(index)">
               删除
             </button> -->
@@ -94,10 +110,16 @@ const delColor = (index: number) => {
           </div>
 
           <div @click="addcolor(true)">
-            <div v-show="!colorPickerFlag" class="colorsItem2 i-carbon:add-alt hover:i-carbon:add-filled" />
+            <div
+              v-show="!colorPickerFlag"
+              class="colorsItem2 i-carbon:add-alt hover:i-carbon:add-filled"
+            />
 
-            <div v-show="colorPickerFlag ">
-              <ColorPicker :color="cacheColor" @changePickerColorBen="selectColor" />
+            <div v-show="colorPickerFlag">
+              <ColorPicker
+                :color="cacheColor"
+                @changePickerColorBen="selectColor"
+              />
             </div>
           </div>
 
--- pickMeColor/src/components/Header.vue
+++ pickMeColor/src/components/Header.vue
@@ -1,23 +1,27 @@
 <script setup>
-import { useDark, useToggle } from '@vueuse/core'
+import { useDark, useToggle } from "@vueuse/core";
 
-const isDark = useDark()
-const toggleDark = useToggle(isDark)
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
 </script>
 
 <template>
   <nav flex="~" justify-between w-cen m-au>
     <!-- <img src="" alt="logo"> -->
     <div>
-      <div font="sans 700" text-60>
-        PickMyColor
-      </div>
+      <div class="font" font="sans 700" text-60>PickMyColor</div>
       <div class="bar" m-au w-cen />
     </div>
+    <button class="githubIcon" w-cen w-right>
+      <a href="https://github.com/2803713783/pickMyColor" target="_blank">
+        <span class="i-carbon:logo-github">git</span>
+        <span>star</span>
+      </a>
+    </button>
     <button class="icon-btn !outline-none" @click="toggleDark()">
       <div v-if="isDark" i-carbon-moon />
       <div v-else i-carbon-sun />
-      <span class="ml-2">{{ isDark ? 'Dark' : 'Light' }}</span>
+      <span class="ml-2">{{ isDark ? "Dark" : "Light" }}</span>
     </button>
   </nav>
 </template>
@@ -27,14 +31,25 @@ const toggleDark = useToggle(isDark)
   margin: auto;
 }
 
+.githubIcon span {
+  padding: 0.5vh;
+}
+
 .bar {
   height: 7px;
   width: 50px;
-  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #6f8681, #D9AFD9, #97D9E1);
+  background: linear-gradient(
+    -45deg,
+    #ee7752,
+    #e73c7e,
+    #23a6d5,
+    #6f8681,
+    #d9afd9,
+    #97d9e1
+  );
   background-size: 700% 700%;
   animation: barcolor 5s ease infinite;
   border-radius: 16px;
-
 }
 
 @keyframes barcolor {
